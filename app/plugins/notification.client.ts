8d999c12b330e6a0167cdb87250807d84e213eb0
Ra13um42 <to@outlook.de>
2025-02-23 14:57:39 +0100

    reconnect websocket

--- app/plugins/notification.client.ts
+++ app/plugins/notification.client.ts
@@ -1,3 +1,4 @@
+import type { RealtimeChannel } from "@supabase/supabase-js"
 import type { Tables } from "~~/types/database.types"
 type Message = Tables<"messages">
 
@@ -10,23 +11,51 @@ export default defineNuxtPlugin(async (nuxtApp) => {
   const user = useSupabaseUser()
   if (!user.value) return
 
-  const channel = supabase.channel("messages")
-  channel
-    .on(
-      "postgres_changes",
-      { event: "INSERT", schema: "public", table: "messages" },
-      async (payload) => {
-        const currentConversation = getCurrentConversation()
-        if (currentConversation && payload.new.sender_id === currentConversation.userId) {
-          currentConversation.addMessage(payload.new as Message)
-          currentConversation.markAsReadAll()
-        } else {
-          fetchUnreadConversationCount()
+  let channel: RealtimeChannel
+
+  const connectChannel = () => {
+    channel = supabase.channel("messages")
+    channel
+      .on(
+        "postgres_changes",
+        { event: "INSERT", schema: "public", table: "messages" },
+        async (payload) => {
+          const currentConversation = getCurrentConversation()
+          if (currentConversation && payload.new.sender_id === currentConversation.userId) {
+            currentConversation.addMessage(payload.new as Message)
+            currentConversation.markAsReadAll()
+          } else {
+            fetchUnreadConversationCount()
+          }
+          if (route.path === "/conversations") {
+            fetchConversations()
+          }
         }
-        if (route.path === "/conversations") {
-          fetchConversations()
-        }
+      )
+      .subscribe()
+  }
+
+  await connectChannel()
+
+  document.addEventListener("visibilitychange", () => {
+    if (document.visibilityState === "visible") {
+      console.log("Tab wieder sichtbar. Überprüfe Verbindung...", new Date().toISOString())
+      if (channel.state !== "joined") {
+        console.log("reconnect")
+        reconnectChannel()
+        fetchUnreadConversationCount()
       }
-    )
-    .subscribe()
+    }
+  })
+
+  async function reconnectChannel() {
+    try {
+      await channel.unsubscribe()
+      await supabase.removeChannel(channel)
+      connectChannel()
+      console.log("Erneut verbunden")
+    } catch (error) {
+      console.error("Fehler beim Reconnect:", error)
+    }
+  }
 })
